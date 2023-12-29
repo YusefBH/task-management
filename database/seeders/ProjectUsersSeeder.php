c8fed7ce842a9691643209482e6ec392cddab56d
unknown <yusefb1382@gmail.com>
2023-12-30 02:59:58 +0330

    create seeder for User and Project and ProjectUsers

---
+++ database/seeders/ProjectUsersSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\Project;
+use App\Models\ProjectUser;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::find(100);
+        for ($i = 1; $i < 4; $i++) {
+            $project = Project::inRandomOrder()->first();
+            $rule = ['OWNER' , 'MEMBER' , 'VIEWER'];
+            ProjectUser::create([
+                'rule' => $rule[rand(0,2)],
+                'user_id' => $user->id,
+                'project_id' => $project->id,
+            ]);
+        }
+
+
+        for ($i = 1; $i < 16; $i++) {
+            $user = User::inRandomOrder()->first();
+            $project = Project::inRandomOrder()->first();
+            $rule = ['OWNER' , 'MEMBER' , 'VIEWER'];
+            ProjectUser::create([
+                'rule' => $rule[rand(0,2)],
+                'user_id' => $user->id,
+                'project_id' => $project->id,
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i < 20; $i++) {
+            User::create([
+                'name' => fake()->name(),
+                'email' => fake()->email,
+                'password' => Hash::make('12345678'),
+            ]);
+        }
+        User::create([
+            "id" => 100,
+            "name" => "yousef",
+            "email" => "yusefbh1382@gamil.com",
+            "password" => Hash::make('12345678'),
+        ]);
+
+    }
+}

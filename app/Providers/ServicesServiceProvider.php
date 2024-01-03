8a4286c5cfc666cfc677e12da2b782e1376747da
unknown <yusefb1382@gmail.com>
2024-01-04 01:49:20 +0330

    create a provider for boot Services

---
+++ app/Providers/ServicesServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServicesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->app->bind(
+            'App\Services\Project\CreateProject\CreateProjectServiceInterface',
+            'App\Services\Project\CreateProject\CreateProjectService'
+        );
+        $this->app->bind(
+            'App\Services\Project\DeleteProject\DeleteProjectServiceInterface',
+            'App\Services\Project\DeleteProject\DeleteProjectService'
+        );
+        $this->app->bind(
+            'App\Services\Project\IndexProject\IndexProjectServiceInterface',
+            'App\Services\Project\IndexProject\IndexProjectService'
+        );
+        $this->app->bind(
+            'App\Services\Project\ShowProject\ShowProjectServiceInterface',
+            'App\Services\Project\ShowProject\ShowProjectService'
+        );
+        $this->app->bind(
+            'App\Services\Project\UpdateProject\UpdateProjectServiceInterface',
+            'App\Services\Project\UpdateProject\UpdateProjectService'
+        );
+    }
+}
